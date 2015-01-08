6035347edb1a406652be647f3f610ce748a09294
John Luo <john.luo@gmail.com>
2015-01-07 16:56:06 -0800

    Initial Commit

---
+++ SmartDoor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartDoor
+//
+//  Created by John Luo on 1/7/15.
+//  Copyright (c) 2015 smart home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
